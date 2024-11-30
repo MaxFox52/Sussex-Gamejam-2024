fdef09270c25a4eaf7aa4ec8e109907be487e1af
MaxFox52 <fordhammax@gmail.com>
2024-11-30 21:31:52 +0000

    properly naming images and also cementing movement for player

---
+++ Glorbbus Gleebbus Must Die/Assets/Scripts/Survival Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int playerHealth = 150;
+    public bool hasJetpack = false;
+    public bool hasLifeline = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
