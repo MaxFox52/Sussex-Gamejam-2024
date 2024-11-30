514adc2852d39398754388ab21015077a15e6143
MaxFox52 <fordhammax@gmail.com>
2024-11-30 18:47:41 +0000

    adding player movement to survival player scripts

---
+++ Glorbbus Gleebbus Must Die/Assets/Scripts/Survival Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // give the player sprite the Character Controller 2D Script firat
+    // give Player Movement after and make the game object (body) the Controller
+
+    // each character needs a top collider and a bottom collider for crouch
+    // character needs box collider 2D on top, circle collider 2D on bottom
+    // each player object needs a blank player object to check ceiling and check ground
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
